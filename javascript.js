6d8145c17a677531893e7d1b36e8f46d4420fa2b
Sezer Ökmen <szrokmn34@gmail.com>
2022-09-16 12:57:33 +0300

    continue

--- javascript.js
+++ javascript.js
@@ -7,10 +7,17 @@ let taskCounter = 0;
 addBtn.addEventListener("click", () => {
     if(textInput.value) {
         const task = textInput.value;
-        
+        taskCounter++;
+        todoDiv.innerHTML += createTask(task, taskCounter);
         textInput.value="";
         textInput.focus();
     }else{
         alert("Please enter text");
     }
-})
+});
+todo
+function createTask(taskText, taskNo){
+    return `<div
+    class="bg-danger rounded text-light d-flex flex-row justify-content-between align-items-center task" id="task-${taskNo}"><span><i class="fa-regular fa-circle"></i> &nbsp;</span>${taskText}Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, voluptates.<button class="btn-close btn-close-white task-close"></button>
+</div>`
+}
